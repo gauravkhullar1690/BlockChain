553819d7262cf08f7735fe4a92064f8ec0354402
Gaurav Khullar <gauravkhullar1690@users.noreply.github.com>
2018-02-24 10:41:34 +0400

    Adding proof of work
    Adding proof of work, so that blocks can't be tempered easily

--- BlockChain.py
+++ BlockChain.py
@@ -9,24 +9,37 @@ import json
 
 class Block:
     
-    def __init__(self, index,timestamp,data,previousHash=''):
+    def __init__(self, index,timestamp,data,previousHash=''):   
+        self.nounce = 0 
         self.index = index
         self.timestamp = timestamp
         self.data = data
         self.previousHash = previousHash
         self.hash = self.calculateHash()
+               
     
     def calculateHash(self):
         hash = hashlib.new('sha256')
-        msg = str(self.index) + self.timestamp + json.dumps(self.data) + self.previousHash
+        msg = str(self.nounce) + str(self.index) + self.timestamp + json.dumps(self.data) + self.previousHash
         hash.update(msg.encode('utf-8'))        
         return hash.hexdigest()
+    
+    def mineBlock(self,difficulty):
+        prefix = "0"
+        for i in range(1,difficulty):
+            prefix = prefix + "0"
+        while self.hash[:difficulty] != prefix:
+            self.nounce +=  1
+            self.hash = self.calculateHash()
+        print("Block mined : " + self.hash)
+            
     
     
 class BlockChain:
     
     def __init__(self):
         self.chain = [self.createGenesisBlock()]
+        self.difficulty = 5
     
     def createGenesisBlock(self):
         return Block(0,"01/01/2018","Genesis Block","0000")
@@ -36,7 +49,7 @@ class BlockChain:
     
     def addBlock(self,newBlock):
         newBlock.previousHash = self.getLatestBlock().hash
-        newBlock.hash = newBlock.calculateHash()
+        newBlock.mineBlock(self.difficulty)
         self.chain.append(newBlock)
         
     def isChainValid(self):
@@ -51,20 +64,12 @@ class BlockChain:
         return True
         
 
-print("Initialzing GauravCoin.......")
-gauravCoin = BlockChain()
-print("Created Genesis Block")
-print("Adding Block 1")
-gauravCoin.addBlock(Block(1,"10/01/2018",{'amount' : 4 }))
-print("Adding Block 2")
-gauravCoin.addBlock(Block(2,"13/01/2018",{'amount' : 8 }))
 
-print("Is chain Valid " + str(gauravCoin.isChainValid()))
-gauravCoin.chain[1].data = {'amount' : 16}
-gauravCoin.chain[1].hash = gauravCoin.chain[1].calculateHash()
-gauravCoin.chain[2].previousHash = gauravCoin.chain[1].calculateHash()
-gauravCoin.chain[2].hash = gauravCoin.chain[2].calculateHash()
-print("Is chain Valid " + str(gauravCoin.isChainValid()))
+gauravCoin = BlockChain()
+print("Mining block 1")
+gauravCoin.addBlock(Block(1,"10/01/2018",{'amount' : 4 }))
+print("Mining block 2")
+gauravCoin.addBlock(Block(2,"13/01/2018",{'amount' : 8 }))
 
 
 
@@ -74,6 +79,6 @@ for coin in gauravCoin.chain:
     coinList.append(coin.__dict__)
    
 jsondata=json.dumps(coinList,sort_keys=False, indent=2)
-print ("chain: " + jsondata)
+#print ("chain: " + jsondata)
     
     
