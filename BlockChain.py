a6c7c4f2ffc2398d922d95e5bdd11a377bde139d
Gaurav Khullar <gauravkhullar1690@users.noreply.github.com>
2018-02-24 13:14:00 +0400

    Revert "Adding mining reward"
    This reverts commit f6e1caf44efa7e4f887d2a49ce1962002d8133c8.

--- BlockChain.py
+++ BlockChain.py
@@ -5,29 +5,22 @@ Created on Fri Feb 23 19:06:39 2018
 @author: Gaurav.khullar
 """
 import hashlib
-from datetime import datetime
-
-class Transaction:
-    
-    def __init__(self,fromAddress,toAddress,amount):
-        self.fromAddress = fromAddress
-        self.toAddress = toAddress
-        self.amount = amount
-
+import json
 
 class Block:
     
-    def __init__(self,timestamp,transactions,previousHash=''):   
+    def __init__(self, index,timestamp,data,previousHash=''):   
         self.nounce = 0 
+        self.index = index
         self.timestamp = timestamp
-        self.transactions = transactions
+        self.data = data
         self.previousHash = previousHash
         self.hash = self.calculateHash()
                
     
     def calculateHash(self):
         hash = hashlib.new('sha256')
-        msg = str(self.nounce) + str(self.timestamp) + str(self.transactions) + self.previousHash
+        msg = str(self.nounce) + str(self.index) + self.timestamp + json.dumps(self.data) + self.previousHash
         hash.update(msg.encode('utf-8'))        
         return hash.hexdigest()
     
@@ -39,23 +32,29 @@ class Block:
             self.nounce +=  1
             self.hash = self.calculateHash()
         print("Block mined : " + self.hash)
-        
+            
+    
     
 class BlockChain:
     
     def __init__(self):
         self.chain = [self.createGenesisBlock()]
+<<<<<<< HEAD
         self.difficulty = 4
         self.pendingTransactions = []
         self.miningReward = 100
         
+=======
+        self.difficulty = 5
+>>>>>>> parent of f6e1caf... Adding mining reward
     
     def createGenesisBlock(self):
-        return Block("01/01/2018","Genesis Block","0000")
+        return Block(0,"01/01/2018","Genesis Block","0000")
     
     def getLatestBlock(self):
         return self.chain[len(self.chain)-1]
     
+<<<<<<< HEAD
     def minPendingTransactions(self,miningRewardAddress):
         block = Block(str(datetime.now()),self.pendingTransactions)
         block.mineBlock(self.difficulty)
@@ -79,6 +78,12 @@ class BlockChain:
                 elif transaction.toAddress == address:
                     balance +=  transaction.amount                
         return balance
+=======
+    def addBlock(self,newBlock):
+        newBlock.previousHash = self.getLatestBlock().hash
+        newBlock.mineBlock(self.difficulty)
+        self.chain.append(newBlock)
+>>>>>>> parent of f6e1caf... Adding mining reward
         
     def isChainValid(self):
         for i in range(1,len(self.chain)):            
@@ -94,19 +99,26 @@ class BlockChain:
 
 
 gauravCoin = BlockChain()
-gauravCoin.createTransaction(Transaction("address1","address2",100))
-gauravCoin.createTransaction(Transaction("address2","address3",50))
+print("Mining block 1")
+gauravCoin.addBlock(Block(1,"10/01/2018",{'amount' : 4 }))
+print("Mining block 2")
+gauravCoin.addBlock(Block(2,"13/01/2018",{'amount' : 8 }))
 
-gauravCoin.minPendingTransactions("gaurav")
 
-gauravCoin.createTransaction(Transaction("address1","address2",100))
-gauravCoin.createTransaction(Transaction("address2","address3",50))
-gauravCoin.minPendingTransactions("gaurav")
-
+<<<<<<< HEAD
 gauravCoin.createTransaction(Transaction("gaurav","address4",50))
 gauravCoin.minPendingTransactions("address4")
 
 print(gauravCoin.getBalanceOfAddress("gaurav"))
+=======
+>>>>>>> parent of f6e1caf... Adding mining reward
 
 
+coinList= []
+for coin in gauravCoin.chain:
+    coinList.append(coin.__dict__)
+   
+jsondata=json.dumps(coinList,sort_keys=False, indent=2)
+#print ("chain: " + jsondata)
+    
     
